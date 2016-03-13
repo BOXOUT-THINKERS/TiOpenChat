7da27f86f9066aa09a8ac988492e9998f8c70185
InGrowth <gimdongwoo@gmail.com>
2016-03-13 15:33:52 +0900

    temporary files clean

--- Resources/iphone/alloy/widgets/nl.fokkezb.toast/styles/global.js
+++
@@ -1,7 +0,0 @@
-function WPATH(s) {
-    var index = s.lastIndexOf("/");
-    var path = -1 === index ? "nl.fokkezb.toast/" + s : s.substring(0, index) + "/nl.fokkezb.toast/" + s.substring(index + 1);
-    return path;
-}
-
-module.exports = [];
